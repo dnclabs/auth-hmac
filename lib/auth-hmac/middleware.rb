c4332bd6f1b0ba60627c352c5280537edc3ec5cc
Ultra Ninja <ninja@engineyard.com>
2010-10-05 03:15:42 -0700

    add the middleware

---
+++ lib/auth-hmac/middleware.rb
@@ -0,0 +1,34 @@
+class AuthHMAC
+  class Middleware
+    def initialize(app, credentials)
+      @app = app
+      @hmac = AuthHMAC.new(credentials)
+    end
+
+    def call(env)
+      dup.call!(env)
+    end
+
+    def call!(env)
+      @env = env
+
+      if @hmac.authenticated?(request)
+        rx = Regexp.new("#{@hmac.service_id} ([^:]+):(.+)$")
+
+        if md = rx.match(@hmac.authorization_header(request))
+          env["hmac-auth.access_key_id"] = md[1]
+        else
+          raise "Unknown request"
+        end
+
+        @app.call(env)
+      else
+        Rack::Response.new("Authorization required", 401).finish
+      end
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+  end
+end
